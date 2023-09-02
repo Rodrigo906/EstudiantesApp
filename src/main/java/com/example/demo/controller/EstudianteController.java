d74fc541687624e967d79311c12e7d7efa7183c8
Rodrigo Calizaya <rcalizayadev@gmail.com>
2023-09-01 22:48:26 -0300

    Correccion de notacion de variables

--- src/main/java/com/example/demo/controller/EstudianteController.java
+++ src/main/java/com/example/demo/controller/EstudianteController.java
@@ -2,10 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.EstudianteDTO;
 import com.example.demo.service.port.EstudianteService;
-import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -26,17 +24,17 @@ public class EstudianteController {
     }
 
     @GetMapping("/{id}")
-    public EstudianteDTO obtenerEstudiante(@PathParam("id") Long id){
+    public EstudianteDTO obtenerEstudiante(@PathVariable Long id){
         return estudianteService.find(id);
     }
 
     @PutMapping("/{id}")
-    public EstudianteDTO actualizarEstudiante(@RequestBody EstudianteDTO estudianteDTO, @PathParam("id") Long id){
+    public EstudianteDTO actualizarEstudiante(@RequestBody EstudianteDTO estudianteDTO, @PathVariable Long id){
         return estudianteService.update(estudianteDTO, id);
     }
 
     @DeleteMapping("{id}")
-    public void eliminarEstudiante(@PathParam("id") Long id){
+    public void eliminarEstudiante(@PathVariable Long id){
         estudianteService.delete(id);
     }
 
--- src/main/java/com/example/demo/controller/InscripcionController.java
+++ src/main/java/com/example/demo/controller/InscripcionController.java
@@ -1,8 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.port.InscripcionService;
-import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +14,7 @@ public class InscripcionController {
     private InscripcionService inscripcionService;
 
     @PostMapping("/{idEstudiante}/{idCurso}")
-    public void registrarInscripcion(@PathParam("idEstudiante") Long idEstudiante,
-                                               @PathParam("idCurso") Long idCurso){
+    public void registrarInscripcion(@PathVariable Long idEstudiante, @PathVariable Long idCurso){
         inscripcionService.registrarInscripcion(idEstudiante, idCurso);
     }
 }
